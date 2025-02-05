008fcb87bae0dbb30b3b84c2bcb87cbe2afb553d
chiji1108 <shingo.pad@gmail.com>
2025-02-05 20:33:18 +0900

    Enhance accessibility for word list in WordScramble

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -116,6 +116,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
