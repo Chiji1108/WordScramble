468f1c5c8861af37bfc6b2e977aae97120d911e6
chiji1108 <shingo.pad@gmail.com>
2025-02-05 20:31:21 +0900

    Refactor code formatting and improve code style in ContentView

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,44 +12,48 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     @State private var score = 0
-    
+
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
-    
+
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
         guard answer != rootWord else { return }
         guard answer.count > 3 else {
-             wordError(title: "Word too short", message: "The word must be at least four letters long.")
+            wordError(
+                title: "Word too short", message: "The word must be at least four letters long.")
             return
         }
-        
+
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original")
             return
         }
 
         guard isPossible(word: answer) else {
-            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            wordError(
+                title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!"
+            )
             return
         }
 
         guard isReal(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            wordError(
+                title: "Word not recognized", message: "You can't just make them up, you know!")
             return
         }
-        
+
         score += answer.count
-        
+
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
-        
+
     }
-    
+
     func startGame() {
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -61,17 +65,17 @@ struct ContentView: View {
                 return
             }
         }
-        
+
         fatalError("Could not lead start.txt from bundle.")
     }
-    
+
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
-    
+
     func isPossible(word: String) -> Bool {
         var tempWord = rootWord
-        
+
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: pos)
@@ -79,24 +83,25 @@ struct ContentView: View {
                 return false
             }
         }
-        
+
         return true
     }
-    
+
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
         return misspelledRange.location == NSNotFound
     }
-    
+
     func wordError(title: String, message: String) {
         errorTitle = title
         errorMessage = message
         showingError = true
     }
-    
+
     var body: some View {
         NavigationStack {
             List {
@@ -104,7 +109,7 @@ struct ContentView: View {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
                 }
-                
+
                 Section("Score: \(score)") {
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
@@ -117,7 +122,8 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
-            .alert(errorTitle, isPresented: $showingError) { } message: {
+            .alert(errorTitle, isPresented: $showingError) {
+            } message: {
                 Text(errorMessage)
             }
             .toolbar {
